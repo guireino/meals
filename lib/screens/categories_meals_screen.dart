9f3ea02176ad986821b12611386fc80954295004
guilherme <guiabucarma@hotmail.com>
2021-09-06 17:25:19 -0400

    add arquivos

---
+++ lib/screens/categories_meals_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+import '../components/meal_item.dart';
+
+import '../models/category.dart';
+import '../models/meal.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  // final Category category;
+
+  // const CategoriesMealsScreen(this.category);
+
+  final List<Meal> meals;
+
+  const CategoriesMealsScreen(this.meals);
+
+  @override
+  Widget build(BuildContext context) {
+    final category = ModalRoute.of(context).settings.arguments
+        as Category; // pegando valor variavel
+
+    final categoryMeals = meals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category.title),
+      ),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(categoryMeals[index]);
+        },
+      ),
+    );
+  }
+}
